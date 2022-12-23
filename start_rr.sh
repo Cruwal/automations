f79a155e7dddee8af76556813374bd3941d4388f
Cruwal <wallace.souza@foxbit.com.br>
2022-12-23 15:02:14 -0300

    update startup automations

--- start_rr.sh
+++ start_rr.sh
@@ -1,18 +1,19 @@
 #! /bin/bash
 
-cd $HOME/Documents/runrunit/
+cd $HOME/Documents/foxbit/blue-whale/
+gnome-terminal --tab --title="blue-whale" -- zsh -c "docker-compose up -d; $SHELL"
 
-gnome-terminal --tab --title="terminal" -- zsh -c "git fetch upstream; $SHELL"
-gnome-terminal --tab --title="foreman" -- zsh -c "foreman start -f foreman start -f Procfile.dev.linux; $SHELL"
-gnome-terminal --tab --title="rails s" -- zsh -c "rails s; $SHELL"
-gnome-terminal --maximize -- bash -c "cd $HOME/Documents/runrunit/; nvim"
+gnome-terminal --tab --title="anglerfish" -- zsh -c "cd ../anglerfish; git fetch origin; $SHELL"
+gnome-terminal --maximize title="nvim" -- bash -c "cd $HOME/Documents/foxbit/anglerfish/; nvim"
 
-firefox 'https://mail.google.com/chat/u/0/#chat/welcome' & disown firefox
-firefox 'https://runrun.it/en-US' & disown firefox
-google-chrome 'https://calendar.google.com/calendar/u/0/r' & disown google-chrome
-google-chrome 'https://meet.google.com/mwq-vnkz-keh?pli=1&authuser=1' & disown google-chrome
+firefox 'https://calendar.google.com/calendar/u/0/r' & disown firefox
+firefox 'https://mail.google.com/mail/u/0/#inbox' & disown firefox
+firefox 'https://github.com/modiax/anglerfish/pulls' & disown firefox
 
+slack & disown slack
 spotify & disown spotify
 
 sleep 30
 firefox 'localhost:3000/' & disown firefox
+
+exit
